4ef5f6c18a94f722fbf9319650b7bcdbf53a7ebd
wajahat0189 <wajahat131992@gmail.com>
2024-08-24 20:18:12 +0500

    ascending order and Reverse array

--- Sorting the array program/app.js
+++ Sorting the array program/app.js
@@ -1,17 +1,28 @@
 var arr = [];
 for (var i =0; i<5 ; i++){
-    var items = prompt('Enter your data');
+    var items = parseInt(prompt('Enter your data'));
     arr[i]=items;
 }
 
 
-alert(arr);
+console.log(arr);
 var arr1 = arr;
+
+console.log('Writting the following array into reverse order') ;
+//var arr = [5,8,10,4,3,7,1,6,2,9];
+console.log(arr1)
+for( var i=0; i< (arr1.length-1)/2; i++){
+    var j = arr1.length-1-i;
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+console.log(arr1);
 
 
 //var arr = [5,8,10,4,3,7,1,6,2,9];
 //alert(arr);
-alert('Arranging in ascending order');
+console.log('Arranging in ascending order');
 for(var i=0; i < arr.length; i++){
     for(var j=i; j < arr.length; j++){
        if( arr[i]> arr[j]){
@@ -22,14 +33,4 @@ for(var i=0; i < arr.length; i++){
        }
     }
 }
-alert(arr);
-alert('Writting the following array into reverse order') ;
-//var arr = [5,8,10,4,3,7,1,6,2,9];
-alert(arr1)
-for( var i=0; i< (arr1.length-1)/2; i++){
-    var j = arr1.length-1-i;
-    var temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-alert(arr1);
+console.log(arr);
